6b062c5a758621d48c2bf013e4fc3dd90db21c0b
miyalab <miyalab.workspace@gmail.com>
2023-06-17 23:14:09 +0900

    update

---
+++ rfanslidar_driver/launch/rfan16.py
@@ -0,0 +1,36 @@
+from inspect import Parameter
+from os.path import join
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_prefix = get_package_share_directory("rfanslidar_driver")
+   
+    container = Node(
+	    package='rclcpp_components',
+		executable='component_container',
+		name = 'lidar_container',
+		emulate_tty = True,
+		output = 'screen'
+	)
+
+    components = LoadComposableNodes(
+        target_container="lidar_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package='rfanslidar_driver',
+                plugin='MiYALAB::ROS2::RFansLiDAR',
+                name='rfans16',
+                parameters=[join(pkg_prefix, "cfg/rfan16.yaml")],
+                extra_arguments=[{'use_intra_process_comms': True}]
+            )
+        ]
+
+    )
+    return LaunchDescription([
+        container,
+        components
+    ])
